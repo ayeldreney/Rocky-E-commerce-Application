bb8dc60897828bf6222830b21d3c98fe6e2e375f
abdal <abdallagamal117@gmail.com>
2023-11-24 00:02:03 +0200

    minor modifications

--- ViewModels/Account/RegisterViewModel.cs
+++ ViewModels/Account/RegisterViewModel.cs
@@ -1,23 +1,29 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Rocky.ViewModels.Account;
 
 public class RegisterViewModel
 {
 	[Required, StringLength(100)]
+	[DisplayName("FirstName")]
 	public string FirstName { get; set; } = string.Empty;
 
 	[Required, StringLength(100)]
+	[DisplayName("LastName")]
 	public string LastName { get; set; } = string.Empty;
 
 	[Required, StringLength(50, MinimumLength = 4)]
+	[DisplayName("UserName")]
 	public string Username { get; set; } = string.Empty;
 
 	[Required]
 	[RegularExpression(@"^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+.[a-zA-Z]{2,4}$", ErrorMessage = "InvalidEmailShouldBeLike")]
+	[DisplayName("Email")]
 	public string Email { get; set; }
 
 	[Required, StringLength(256)]
 	[DataType(DataType.Password)]
+	[DisplayName("Password")]
 	public string Password { get; set; } = string.Empty;
 }
